639b7eb631c8a90d523a6b0dcb9bf7ed770fd051
gwillmer <gwillmer@users.noreply.github.com>
2021-06-07 13:59:23 -0400

    hmm

--- selfdrive/monitoring/dmonitoringd.py
+++ selfdrive/monitoring/dmonitoringd.py
@@ -38,8 +38,7 @@ def dmonitoringd_thread(sm=None, pm=None):
       driver_engaged = len(sm['carState'].buttonEvents) > 0 or \
                         v_cruise != v_cruise_last or \
                         sm['carState'].steeringPressed or \
-                        sm['carState'].gasPressed or \
-                        opParams().get('not_visible_to_dm_cam')
+                        sm['carState'].gasPressed
       if driver_engaged:
         driver_status.update(Events(), True, sm['controlsState'].enabled, sm['carState'].standstill)
       v_cruise_last = v_cruise
