52c1948c4edfce67a41d4b4e144cc00c5dfaeadf
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2021-08-13 19:59:50 -0400

    Update interface.py

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -51,19 +51,19 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kfV = [0.00005]   # full torque for 10 deg at 80mph means 0.00007818594
     else:
       ret.lateralTuning.pid.kpBP = [0., 10., 30.]
-      ret.lateralTuning.pid.kpV = [0.03, 0.04, 0.05]
+      ret.lateralTuning.pid.kpV = [0.05, 0.06, 0.06]
 
       ret.lateralTuning.pid.kiBP = [0., 30.]
-      ret.lateralTuning.pid.kiV = [0.03, 0.04]
+      ret.lateralTuning.pid.kiV = [0.02, 0.03]
 
       ret.lateralTuning.pid.kdBP = [0.]
-      ret.lateralTuning.pid.kdV = [1000.]
+      ret.lateralTuning.pid.kdV = [1.]
 
       ret.lateralTuning.pid.kfBP = [0., 15., 30.]
-      ret.lateralTuning.pid.kfV = [0.000015, 0.000015, 0.000015]   # full torque for 10 deg at 80mph means 0.00007818594
+      ret.lateralTuning.pid.kfV = [0.000015, 0.000025, 0.000035]   # full torque for 10 deg at 80mph means 0.00007818594
 
-    ret.steerActuatorDelay = 0.01
-    ret.steerRateCost = 0.7
+    ret.steerActuatorDelay = 0.2
+    ret.steerRateCost = 0.5
     ret.steerLimitTimer = 0.7
 
     if candidate in (CAR.JEEP_CHEROKEE, CAR.JEEP_CHEROKEE_2019):
