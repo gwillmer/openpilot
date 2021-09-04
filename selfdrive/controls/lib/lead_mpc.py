562b83a9b16a75720e85d161084147fe6edd85c2
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2021-09-04 12:58:00 -0400

    Update lead_mpc.py

--- selfdrive/controls/lib/lead_mpc.py
+++ selfdrive/controls/lib/lead_mpc.py
@@ -11,7 +11,7 @@ from selfdrive.swaglog import cloudlog
 MPC_T = list(np.arange(0,1.,.2)) + list(np.arange(1.,10.6,.6))
 
 Bpx = [0.75, 1., 1.5]
-BpTR = [1.8, 1.5, 0.7]
+BpTR = [1.8, 1.5, 1.3]
 
 BpV = [0., .278, 3., 8., 15., 30]
 BpXTR = [4., 4., 6., 16., 20., 25.]
