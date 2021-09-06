8a2e58af4b01e986540fb4ae432ec0325edb33f8
gwillmer <gwillmer@users.noreply.github.com>
2021-09-05 22:18:18 -0400

    Update drive_helpers.py

--- selfdrive/controls/lib/drive_helpers.py
+++ selfdrive/controls/lib/drive_helpers.py
@@ -68,10 +68,7 @@ def initialize_v_cruise(v_ego, buttonEvents, v_cruise_last):
 
 def get_lag_adjusted_curvature(CP, v_ego, psis, curvatures, curvature_rates):
 
-  if not Params().get_bool('ChryslerMangoLat'):
-    sad = interp(v_ego, sadBP, sadV)
-  else:
-    sad = 0.2
+  sad = interp(v_ego, sadBP, sadV)
 
   if len(psis) != CONTROL_N:
     psis = [0.0 for i in range(CONTROL_N)]
