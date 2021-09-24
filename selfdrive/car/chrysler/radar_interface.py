36bb5549a9d2e6246b7f38bdf96e1f7b39652620
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2021-09-24 11:25:19 -0400

    Update radar_interface.py

--- selfdrive/car/chrysler/radar_interface.py
+++ selfdrive/car/chrysler/radar_interface.py
@@ -84,7 +84,7 @@ class RadarInterface(RadarInterfaceBase):
       if 'LONG_DIST' in cpt:  # c_* message
         azimuth = math.radians(cpt['LAT_ANGLE'])
         self.pts[trackId].dRel = math.cos(azimuth) * cpt['LONG_DIST']
-        self.pts[trackId].yRel = 0.5 * -math.sin(azimuth) * cpt['LONG_DIST']
+        self.pts[trackId].yRel = -math.sin(azimuth) * cpt['LONG_DIST']
       else:  # d_* message
         self.pts[trackId].vRel = cpt['REL_SPEED']
         self.pts[trackId].measured = bool(cpt['MEASURED'])
